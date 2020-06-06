bd8709d9ab79422462ef4db0a79fa4d12ece77fd
TERRYYYY <kariuki.terry11@gmail.com>
2020-06-06 22:00:39 +0300

    connecting our application to Postgres

--- me/settings.py
+++ me/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'me',
+        'USER': 'terry',
+    'PASSWORD':'2002',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
